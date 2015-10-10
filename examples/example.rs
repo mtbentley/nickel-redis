35e19c15497539ca2f2a4327f7f1ec287ce4627d
Matthew Bentley <matthew.t.bentley@gmail.com>
2015-10-10 12:45:57 -0400

    Update example

--- examples/example.rs
+++ examples/example.rs
@@ -1,13 +1,15 @@
+#![feature(core)]
 extern crate r2d2;
 extern crate redis;
-extern crate openssl;
 #[macro_use] extern crate nickel;
 extern crate nickel_redis;
+extern crate core;
 
 use std::env;
 use r2d2::NopErrorHandler;
 use nickel::{Nickel, HttpRouter};
 use nickel_redis::{RedisMiddleware, RedisRequestExtensions};
+use core::ops::Deref;
 
 fn main() {
     let mut app = Nickel::new();
@@ -18,10 +20,10 @@ fn main() {
                                          Box::new(NopErrorHandler)).unwrap();
     app.utilize(dbpool);
     app.get("/my_counter", middleware! { |request|
-        let _connection = request.redis_conn();
-        println!("{:?}", _connection);
+        let pool_con = request.redis_conn();
+        let con = pool_con.deref();
 
-        // use connection
+        // Use con as your connection
     });
 
     app.get("**", middleware! { println!("!!!") });
